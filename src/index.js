2c576892ecc831d73a54c5cbdc35a2c50118833b
ebisLab <ebaiden@techie.com>
2021-02-11 04:33:55 -0500

    fetch url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("sup")
+// console.log("sup")
+// import axios from 'axios'
+const axios = require('axios')
+
+axios(`http://localhost:8000/about/`)
+.then(res=>res)
+.then(html=>console.log("html", html))
+
+const crawl = ({url})=>{
+}
