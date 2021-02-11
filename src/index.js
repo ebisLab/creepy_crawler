9126084255d679b9147e3bbc2fb6485fe8974679
ebisLab <ebaiden@techie.com>
2021-02-11 11:14:01 -0500

    parse through html to find various elements

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ const cheerio=require("cheerio")
 
 const crawl = async ({url})=>{
     const res = await axios(`http://localhost:8000/about/`)
-    const html = await res
+    const html = await res.data
     console.log("html", html);
+    //created cheerio lib to create an obj. to parse the html that we got back
+    const $ = cheerio.load(html)
+
+    //query for all the links similar to jquery
+
+    //pass tag -> a
+    //.map similar to js function
+    //do a .get on this collection  to convert to normal js array
+    const tags = $('a').map((index,tag)=>tag.attribs.href).get()
+    console.log("tags-->", tags)
+
 }
 
 crawl({
     url:`http://localhost:8000/about/`
 })
+
 
