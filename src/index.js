1c7f431ff57b0c3815f3aedfb3eab0faa7c18b10
ebisLab <ebaiden@techie.com>
2021-02-11 12:30:47 -0500

    links successfully being crawled

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 const axios=require("axios")
 const cheerio=require("cheerio")
+const endpnt= ""
+// const linkUrl= `http://localhost:8000/`
+const linkUrl= `http://godchecker.com`
+
+
+const seenUrl={
+
+}
+//helper fn that pass link and get the full url back
+const  getUrl= (link)=>{
+    //return full url 
+    //if link that comes in doesnt have an http in it 
+
+    if(link.includes('http') !== -1){
+        return link //there's probably more edge cases i need to cover
+    } 
+    else if(link.startsWith("/")){
+        return linkUrl+link
+    }
+    else {
+        return linkUrl+link
+    }
+}
 
 const crawl = async ({url})=>{
-    const res = await axios(`http://localhost:8000/about/`)
+    
+try{    if(seenUrl[url]) return;
+    console.log("crawling-->", url)
+    seenUrl[url] = true;
+    const res = await axios(linkUrl+endpnt)
     const html = await res.data
-    console.log("html", html);
+    // console.log("html", html);
     //created cheerio lib to create an obj. to parse the html that we got back
     const $ = cheerio.load(html)
 
@@ -14,12 +41,29 @@ const crawl = async ({url})=>{
     //.map similar to js function
     //do a .get on this collection  to convert to normal js array
     const tags = $('a').map((index,tag)=>tag.attribs.href).get()
-    console.log("tags-->", tags)
+    // console.log("tags-->", tags)
+    tags.forEach(tag=>{
+        crawl({url:getUrl(tag)})
+    })
+
+    //recursively keep on crawling to get all the links that are in the web page
+    //depth first search 
+    //when you go to the page, click on the link, download that page, and the links on that page
+
+    crawl({url:linkUrl+endpnt})
+
+    // const err = await Promise.reject(new Error("whoops!"))
+
+}
+catch(err){
+    console.log('That did not go so well', err)
+    throw err
+}
 
 }
 
 crawl({
-    url:`http://localhost:8000/about/`
+    url: linkUrl
 })
 
 
