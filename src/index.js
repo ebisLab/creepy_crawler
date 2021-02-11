4ec7f4d6177557c537dd1983346bd40d25b73b49
ebisLab <ebaiden@techie.com>
2021-02-11 11:07:07 -0500

    refactor fetching html

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-// console.log("sup")
-// import axios from 'axios'
-const axios = require('axios');
-const cheerio =require("cheerio")
+const axios=require("axios")
+const cheerio=require("cheerio")
 
-axios(`http://localhost:8000/about/`)
-.then(res=>res)
-.then(html=>{
-    // console.log("html", html)
-    const $=cheerio.load(html.data)
-const links= $('img').map((i, link)=> link.attribs.alt).get(); //convert it to the array
-console.log("links", links)
+const crawl = async ({url})=>{
+    const res = await axios(`http://localhost:8000/about/`)
+    const html = await res
+    console.log("html", html);
+}
+
+crawl({
+    url:`http://localhost:8000/about/`
 })
-// const $=cheerio.load('<h2 class="title">Hello world</h2>')
 
-// const links= $('a').map((i, link)=> link.attribs.href).get(); //convert it to the array
-// const stuff=$(h2.title).text('Hello there');
-// $.html()
-
-// console.log("links", links)
-
-console.log("stuff")
-
-// const crawl = ({url})=>{
-// }
