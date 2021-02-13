8346da95dab9a6dcba9f67c70c536a1190613c8d
ebisLab <ebaiden@techie.com>
2021-02-13 07:44:58 -0500

    crawler works for local

--- src/index.js
+++ src/index.js
@@ -1,69 +1,91 @@
-const axios=require("axios")
-const cheerio=require("cheerio")
-const endpnt= ""
-// const linkUrl= `http://localhost:8000/`
-const linkUrl= `http://godchecker.com`
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const fs = require('fs');
+const path =require('path')
+// const siteUrl ="http://localhost:4000/"
+// const siteUrl="https://blisswalls.netlify.app"
+// const siteUrl="https://ijeamaka-anyene.netlify.app/"
 
 
-const seenUrl={
 
-}
-//helper fn that pass link and get the full url back
-const  getUrl= (link)=>{
-    //return full url 
-    //if link that comes in doesnt have an http in it 
 
-    if(link.includes('http') !== -1){
-        return link //there's probably more edge cases i need to cover
-    } 
-    else if(link.startsWith("/")){
-        return linkUrl+link
+
+const seenUrl={}
+const getUrl=(link)=>{
+    if (link.includes('http')){
+        return link
+    }else if(link.startsWith("/")){
+        return `http://localhost:5000${link}`
     }
-    else {
-        return linkUrl+link
+    else{
+        return `http://localhost:5000/${link}`
     }
 }
 
 const crawl = async ({url})=>{
+
+    try{
+        if(seenUrl[url])return;
+        // console.log('crawling', url)
+        seenUrl[url]=true;
+        // console.log('Crawling', url)
+        const {data} = await axios.get(url)
+        // console.log('data', data)
+        const $ =cheerio.load(data)
+        const links = $('a').map((i,tag)=>tag.attribs.href).get()
+        const imageUrl = $('img').map((index,tag)=>tag.attribs.src).get()
+
+        // console.log(imageUrl)
+
+        links.forEach(link=>{crawl({url:getUrl(link)})})
+        // imageUrl.forEach(link=>{crawl({url:getUrl(link)})})
+        imageUrl.forEach(imglink=> {
+            console.log("imglink", getUrl(imglink))
+
+            axios({
+                method: 'get',
+                url: getUrl(imglink),
+                responseType: 'stream'
+              })
+                .then((res)=> {
+                    // console.log("res")
+                const filename=path.basename(imageUrl.toString())
+                // console.log(`images/${filename}`)
+                  res.data.pipe(fs.createWriteStream(`images/${filename}`))
+                });
+
+            // axios.get(getUrl(imglink)).then(res=>{
+            //     const dest=fs.createWriteStream('images/fortune.jpg');
+            //     console.log("dest", res.data.pipe)
+            //     res.data.pipe(dest)
+            // })
+        })
+
+
+        // imageUrl.forEach(imageurl=>{
+        //     axios.post(getUrl(imageurl)).then(res=>{
+        //         const dest = fs.createWriteStream("images/myimage.jpg")
+        //     //    console.log("res-->",res)
+        //     //    res.data.pipe(dest)
+        //     })
+        // })
+
+        // console.log("$$$$", images)
+
+        // const links = $('a').map((index,tag)=>tag.attribs.href).get()
+        // links.forEach(link=>{crawl({url:getUrl(link)})})
+        // console.log("$$$$", links)
+
+
+
     
-try{    if(seenUrl[url]) return;
-    console.log("crawling-->", url)
-    seenUrl[url] = true;
-    const res = await axios(linkUrl+endpnt)
-    const html = await res.data
-    // console.log("html", html);
-    //created cheerio lib to create an obj. to parse the html that we got back
-    const $ = cheerio.load(html)
 
-    //query for all the links similar to jquery
-
-    //pass tag -> a
-    //.map similar to js function
-    //do a .get on this collection  to convert to normal js array
-    const tags = $('a').map((index,tag)=>tag.attribs.href).get()
-    // console.log("tags-->", tags)
-    tags.forEach(tag=>{
-        crawl({url:getUrl(tag)})
-    })
-
-    //recursively keep on crawling to get all the links that are in the web page
-    //depth first search 
-    //when you go to the page, click on the link, download that page, and the links on that page
-
-    crawl({url:linkUrl+endpnt})
-
-    // const err = await Promise.reject(new Error("whoops!"))
-
-}
-catch(err){
-    console.log('That did not go so well', err)
-    throw err
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
-}
 
-crawl({
-    url: linkUrl
-})
-
-
+crawl({url:"http://localhost:5000"})
